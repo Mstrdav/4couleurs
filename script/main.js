9e1777ce27a6a0500368e41de4afa5390e31a0e0
Colin Geindre <colin.geindre@gmail.com>
2019-12-12 19:10:29 +0100

    Selection des couleurs fonctionnelle !

--- script/main.js
+++ script/main.js
@@ -1,14 +1,112 @@
-// COLOR SELECTOR
-var color_selector = document.getElementById('color_selector');
+// COLOR PICKER
 var red = document.getElementById('red');
 var blue = document.getElementById('blue');
 var green = document.getElementById('green');
 var yellow = document.getElementById('yellow');
 
-var zone1 = document.getElementById('zone1');
+// SELECTED COLOR
+var selectedColor = 0;
 
-zone1.addEventListener('click', function (event) {
-    color_selector.style.opacity = '90%';
-    color_selector.style.top = event.clientY.toString()+'px';
-    color_selector.style.left = event.clientX.toString()+'px';    
+// ZONES
+var zone1 = document.getElementById('zone1');
+var zone2 = document.getElementById('zone2');
+var zone3 = document.getElementById('zone3');
+var zone4 = document.getElementById('zone4');
+
+
+// Events on Zones
+zone1.addEventListener('click', function(event) {
+    fill(this, selectedColor);
 });
+
+zone2.addEventListener('click', function(event) {
+    fill(this, selectedColor);
+});
+
+zone3.addEventListener('click', function(event) {
+    fill(this, selectedColor);
+});
+
+zone4.addEventListener('click', function(event) {
+    fill(this, selectedColor);
+});
+
+// Events on ColorPicker
+blue.addEventListener('click', function (event) {
+        document.body.classList = "";
+    if (selectedColor != 1) {
+        remove(selectedColor);
+        document.body.classList.add("blueCursor");
+        selectedColor = 1;
+        blue.classList.add("selected");
+    } else {
+        selectedColor = 0;
+        blue.classList.remove("selected");
+    }
+});
+
+red.addEventListener('click', function (event) {
+    document.body.classList = "";
+    if (selectedColor != 2) {
+        remove(selectedColor);
+        document.body.classList.add("redCursor");
+        selectedColor = 2;
+        red.classList.add("selected");
+    } else {
+        selectedColor = 0;
+        red.classList.remove("selected");
+    }
+});
+
+green.addEventListener('click', function (event) {
+        document.body.classList = "";
+    if (selectedColor != 3) {
+        remove(selectedColor);
+        document.body.classList.add("greenCursor");
+        selectedColor = 3;
+        green.classList.add("selected");
+    } else {
+        selectedColor = 0;
+        green.classList.remove("selected");
+    }
+});
+
+yellow.addEventListener('click', function (event) {
+    document.body.classList = "";
+    if (selectedColor != 4) {
+        remove(selectedColor);
+        document.body.classList.add("yellowCursor");
+        selectedColor = 4;
+        yellow.classList.add("selected");
+    } else {
+        selectedColor = 0;
+        yellow.classList.remove("selected");
+    }
+});
+
+var remove = function(colorToRemove) {
+    if(colorToRemove==0) {
+    } else if(colorToRemove==1) {
+        blue.classList.remove("selected");
+    } else if(colorToRemove==2) {
+        red.classList.remove("selected");
+    } else if(colorToRemove==3) {
+        green.classList.remove("selected");
+    } else {
+        yellow.classList.remove("selected");
+    }
+}
+
+var fill = function(zone, colorToFill) {
+    if(colorToFill==0) {
+        zone.style.fill = "white";
+    } else if(colorToFill==1) {
+        zone.style.fill = "blue";
+    } else if(colorToFill==2) {
+        zone.style.fill = "red";
+    } else if(colorToFill==3) {
+        zone.style.fill = "green";
+    } else {
+        zone.style.fill = "yellow";
+    }
+}
